e0505a0e1df8603c794293f49610c017ca3e21c5
PRINCE TECH <majidirwanda815@gmail.com>
2025-05-29 02:18:45 +0300

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -3,7 +3,7 @@ const { sleep } = require("../lib/functions");
 
 cmd({  
     pattern: "restart",  
-    desc: "Restart TOHID_MD",  
+    desc: "Restart PRINCE_MD",  
     category: "owner",  
     filename: __filename  
 },  
